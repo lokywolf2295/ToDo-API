e0023904dd44b84b0eac09d5f0073f761ad9c9f9
lokywolf2295 <matiasesek1995@gmail.com>
2023-02-10 19:56:34 -0300

    Update: Create application.yml and TaskController class

---
+++ src/main/java/com/sacavix/todoapp/controller/TaskController.java
@@ -0,0 +1,8 @@
+package com.sacavix.todoapp.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController /*Anotación para confirmar de que se trata de un controlador el cual será revisado por swagger*/
+public class TaskController {
+
+}
