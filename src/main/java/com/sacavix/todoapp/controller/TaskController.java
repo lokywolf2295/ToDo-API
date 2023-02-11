455ff3b56b02850b19163c8e08c2bfe130ec020c
lokywolf2295 <matiasesek1995@gmail.com>
2023-02-11 10:00:35 -0300

    Update: Add createTask PostMapping to conect the controller layer with the service layer

--- src/main/java/com/sacavix/todoapp/controller/TaskController.java
+++ src/main/java/com/sacavix/todoapp/controller/TaskController.java
@@ -1,8 +1,25 @@
 package com.sacavix.todoapp.controller;
 
+import com.sacavix.todoapp.persistence.entity.Task;
+import com.sacavix.todoapp.service.TaskService;
+import com.sacavix.todoapp.service.dto.TaskInDTO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController /*Anotación para confirmar de que se trata de un controlador el cual será revisado por swagger*/
-public class TaskController {
+@RequestMapping("/tasks")
+public class TaskController { //esta capa solo se comunica con la capa de servicio
 
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody TaskInDTO taskInDTO) {
+        return this.taskService.createTask(taskInDTO);
+    }
 }
